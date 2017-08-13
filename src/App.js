6c310a5a7a3a2e5e6ef225852aa6ee61f6c63663
Weslley Rocha <weslleytato.12@gmail.com>
2017-08-13 05:40:10 -0300

    Finished
    Add animation
    Add logic to show description
    Add logic to hide the description when the selected item is tapped again
    Updated to react-native v0.47

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, {Component} from 'react';
-import {View} from 'react-native';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import reducers from './reducers';
-import {Header} from './components/common';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList'
 
 class App extends Component {
     render() {
         return (
             <Provider store={createStore(reducers)}>
                 <View>
-                    <Header title="Tech Stack"/>
+                    <Header title="Tech Stack" />
+                    <LibraryList/>
                 </View>
             </Provider>
         )
